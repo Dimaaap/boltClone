7bf26a259563562b7baa489d907e48f4635d17db
Dimaaap <procdima49@gmail.com>
2023-11-28 23:43:49 +0300

    Add a become partner section

--- bolt_clone/index_page/urls.py
+++ bolt_clone/index_page/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', index_page_view, name="index_page")
+    path('', index_page_view, name="index_page"),
+    path('partner', become_partner_view, name="become_partner")
 ]
--- bolt_clone/index_page/views.py
+++ bolt_clone/index_page/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index_page_view(request):
     return render(request, 'index_page/index_page.html')
+
+
+def become_partner_view(request):
+    return render(request, "index_page/become_partner_page.html")
