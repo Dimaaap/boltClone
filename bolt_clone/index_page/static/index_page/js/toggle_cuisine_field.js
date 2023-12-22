1106bdba308a9a96da1d5f44a2b281e71889d9d4
Dimaaap <procdima49@gmail.com>
2023-12-22 23:41:01 +0300

    Fixed bug with country code input fueld

--- bolt_clone/index_page/static/index_page/js/toggle_cuisine_field.js
+++ bolt_clone/index_page/static/index_page/js/toggle_cuisine_field.js
@@ -1,11 +1,24 @@
 const nicheField = document.getElementById("id_partner_niche");
 const cuisineField = document.querySelector(".conditional-field");
+const countryPhoneCodeField = document.getElementById("id_country_phone_code");
+const countryPhoneCodeArrow = document.getElementById("bottom-arrow-down");
+const selectNicheArrow = document.querySelector()
+
+let currentCountryPhonePos = parseInt(getComputedStyle(countryPhoneCodeField).top, 10)
 
 
 nicheField.addEventListener("change", () => {
     if(nicheField.value !== "Заклад харчування") {
         cuisineField.style.display = 'none';
+        let newPos = currentCountryPhonePos - 100;
+        countryPhoneCodeField.style.top = newPos + "px";
+        let newPosArr = currentCountryPhonePos - 75;
+        countryPhoneCodeArrow.style.top = newPosArr + "px";
     } else {
         cuisineField.style.display = 'block';
+        let newPos = currentCountryPhonePos + 0;
+        countryPhoneCodeField.style.top = newPos + "px";
+        let newPosArr = currentCountryPhonePos + 25;
+        countryPhoneCodeArrow.style.top = newPosArr + "px";
     }
 })
