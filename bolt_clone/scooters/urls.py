6224962f8e11fbecf4cd977a98510f069be6ef94
Dimaaap <procdima49@gmail.com>
2024-02-02 09:31:58 +0300

    Add a safety lab page

--- bolt_clone/scooters/urls.py
+++ bolt_clone/scooters/urls.py
@@ -4,5 +4,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('safety', scooter_safety_page, name="scooter_safety")
+    path('safety', scooter_safety_page, name="scooter_safety"),
+    path('safety-lab', scooter_safety_lab_page, name="scooter_safety_lab"),
+    path('report', scooter_report_page, name='scooter_report')
 ]
--- bolt_clone/scooters/views.py
+++ bolt_clone/scooters/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def scooter_safety_page(request):
     return render(request, 'scooters/safety_page.html')
+
+
+def scooter_safety_lab_page(request):
+    return render(request, 'scooters/safety_lab.html')
+
+
+def scooter_report_page(request):
+    return render(request, 'scooters/report_page.html')
