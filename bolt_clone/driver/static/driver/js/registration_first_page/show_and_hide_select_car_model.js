34f1a08f79912c3794cc97ba305a493055d21c3c
Dimaaap <procdima49@gmail.com>
2024-02-23 20:00:19 +0300

    Add a pipe in fields

--- bolt_clone/driver/static/driver/js/registration_first_page/show_and_hide_select_car_model.js
+++ bolt_clone/driver/static/driver/js/registration_first_page/show_and_hide_select_car_model.js
@@ -2,11 +2,13 @@ const carModelSelectField = document.getElementById("driver-model-car-select");
 const carSelectField = document.getElementById("driver-car-select")
 const carModelSelectFieldSmall = Array.from(document.querySelectorAll(".not-found-car-model"));
 const xMarks = Array.from(document.querySelectorAll(".xmark-span"));
+const carModelSelectPipe = document.getElementById("car-model-field-pipe");
 
 document.addEventListener("DOMContentLoaded", () => {
    carModelSelectField.style.display = "none";
    carModelSelectFieldSmall[1].style.display = "none";
    xMarks[0].style.top = "25%";
+   carModelSelectPipe.style.display = "none";
 
    carSelectField.addEventListener("input", () => {
         carModelSelectField.style.display = "block";
@@ -14,5 +16,6 @@ document.addEventListener("DOMContentLoaded", () => {
         carModelSelectFieldSmall[0].style.display = "none";
         xMarks[0].style.top = "45%";
         xMarks[1].style.top = "25%";
+        carModelSelectPipe.style.display = "inline-block";
    })
 })
