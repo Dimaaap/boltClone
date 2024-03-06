4fbd8f8a9816b196b9a45c7015611920ab15716b
Dimaaap <procdima49@gmail.com>
2024-03-06 23:47:07 +0300

    Add file form

---
+++ bolt_clone/driver/static/driver/js/registration_second_page/custom_file_downloader.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const fileUploadWrappers = document.querySelectorAll(".file-upload-wrapper");
+
+    fileUploadWrappers.forEach(wrapper => {
+        const fileInput = wrapper.querySelector("input[type='file']");
+        const button = wrapper.querySelector(".custom-file-downloader");
+
+        button.addEventListener("click", () => {
+            fileInput.click();
+        });
+    })
+})
